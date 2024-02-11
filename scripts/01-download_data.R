ba6d8b802e25401d04559023960d0785ecbb4dde
faralk <156137245+faralk@users.noreply.github.com>
2024-02-11 17:19:07 -0500

    Download data
    Provided detailed instructions on how to download the data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,17 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Harvard's dataverse 
+# Author: Fares Alkorani
+# Date: 11 February 2024
+# Contact: 156137245+faralk@users.noreply.github.com
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
-
-#### Workspace setup ####
-library(opendatatoronto)
-library(tidyverse)
-# [...UPDATE THIS...]
+# Pre-requisites: -
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
 
-
-
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+# Unfortunately, the data cannot be downloaded automatically.
+# Thus, you can take the following the steps to download the data.
+# 1) First, visit https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/BFEBHQ.
+# 2) Then, press the 'Access Dataset' button.
+# 3) Press "Download ZIP (384.2 KB).
 
          
